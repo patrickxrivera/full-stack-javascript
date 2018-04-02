7bf5e3919265b5ebea34b554376a4259e678aaf2
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-01 23:46:40 -0400

    Added keys file

---
+++ build/config/keys.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var keys = {
+  googleClientID: '1061982077695-tblsestgi3ml7bolqoihtsk580qfe6d2.apps.googleusercontent.com',
+  googleClientSecret: 'Nyd1mFivdQIbo7KxCDXw4Dsl',
+  mongoURI: 'mongodb://patrick:password@ds231229.mlab.com:31229/emaily-dev',
+  cookieKey: 'kfasjd;sajf;kdsjak;fjew;qkljak;sdjfkl;jadsk;lfjk;dsaj'
+};
+
+exports.default = keys;
+//# sourceMappingURL=keys.js.map
